e186cf82505b3fa1eabf17b0d2857b9cb4f5a226
r3dmaohong <r3dmaohong@gmail.com>
2016-11-17 17:30:37 +0800

    upd

--- rscript/DStatUniverityPath.R
+++ rscript/DStatUniverityPath.R
@@ -14,12 +14,13 @@ options(scipen=999)
 dir.create(file.path("output", Sys.Date()), showWarnings = FALSE)
 
 ##libraries
-options(java.parameters = "-Xmx2g")
+options(java.parameters = "-Xmx4g")
 library(readxl)
 library(data.table)
 library(dplyr)
 library(stringdist)
 library(XLConnect)
+library(magrittr)
 
 ##
 #insertRow <- function(existingDF, newrow, r) {
@@ -34,9 +35,13 @@ n              <- readline(prompt="Enter the folder's name: ") ##list.files("inp
 totalFormFName <- list.files(file.path("input", n), full=T)[grepl("科系職涯地圖", list.files(file.path("input", n))) & !grepl("~$", list.files(file.path("input", n)), fixed = TRUE)]
 
 ##Standard data
-MatchTable    <- read_excel(totalFormFName, sheet = "學系相關資料")
-OldEducation  <- read_excel(totalFormFName, sheet = "升學藍圖")
-OldEmployment <- read_excel(totalFormFName, sheet = "就業藍圖")
+wb         <- loadWorkbook(totalFormFName)
+MatchTable <- readWorksheet(wb, sheet = "學系相關資料", header = TRUE)
+OldEducation <- readWorksheet(wb, sheet = "升學藍圖", header = TRUE)
+OldEmployment <- readWorksheet(wb, sheet = "就業藍圖", header = TRUE)
+#MatchTable    <- read_excel(totalFormFName, sheet = "學系相關資料")
+#OldEducation  <- read_excel(totalFormFName, sheet = "升學藍圖")
+#OldEmployment <- read_excel(totalFormFName, sheet = "就業藍圖")
 
 ##Raw data
 EducationFName      <- list.files(file.path("input", n), full=T)[grepl("升學", list.files(file.path("input", n)))]
@@ -69,7 +74,7 @@ Employment <- Employment[!grepl("[0-9]", 學校名稱)]
 Employment <- Employment[!grepl("學分班", 科系名稱)]
 
 ##College names correction...
-Employment <- Employment[學校名稱 %in% MatchTable$學校名稱]
+Employment <- Employment[(學校名稱 %>% iconv("UTF-8")) %in% (MatchTable$學校名稱 %>% iconv("UTF-8") %>% unique)]
 Employment$SchoolName <- Employment$學校名稱
 #for(x in unique(Employment$學校名稱)){
 #  tmp <- ifelse(toString(rev(sort(CollegeNameT$對應表[CollegeNameT$trim後原始 == x ]))[1])!="" & toString(rev(sort(CollegeNameT$對應表[CollegeNameT$trim後原始== x]))[1])!="NA"
@@ -106,9 +111,6 @@ for(i in 1:nrow(EmploymentMatchT)){
   cat("\r Department Correction : ", format(round(i/nrow(EmploymentMatchT)*100, 2), nsmall=2), " %")
 }
 
-##科系名稱沒正規化道!
-
-
 ##########
 ## Module
 ##########
@@ -135,28 +137,28 @@ DStatModule <- function(DT, school, department, target){
 #ODT=OldEmployment
 #NDT=DStatDT
 #switchV= "學校" "產業" "職務"
-UpdateModule <- function(ODT, NDT, target, switchV=""){
+UpdateModule <- function(NDT, ODT, target, switchV=""){
+  print(paste0(switchV, ": ", target))
   setDT(ODT)
   setDT(NDT)
   
-  names(ODT) <- gsub("[()（）]", "_", names(ODT))
-  target     <- gsub("[()（）]", "_", target)
+  names(ODT) <- gsub("[()（）]", ".", names(ODT))
+  target     <- gsub("[()（）]", ".", target)
   
   if(switchV=="產業" | switchV=="職務"){
-    names(ODT)[which(names(ODT)=="類別_0=職務; 1=產業_")] <- "類別"
+    names(ODT)[which(names(ODT)=="類別.0.職務..1.產業.")] <- "類別"
     if(switchV=="產業"){
       switchV <- 1
     }else{
       switchV <- 0
     }
   }
-  
-  ##以新的為基礎? 並完再merge?
-  ##or 直接加在old? => rbind後重新計算? =>取前10與其他
+
   SODT <- ODT[類別==0, c("學校名稱", "科系名稱", target, names(ODT) %>% .[which(.==target)+1]), with=F]
   NDT
   names(NDT) <- c("學校名稱", "科系名稱", target, names(ODT) %>% .[which(.==target)+1])
   
+  print(" Merging New and old data...")
   TDT      <- rbind(NDT, SODT)
   #tmp_eval <- paste0(names(ODT) %>% .[which(.==target)+1], ":=sum(as.numeric(", names(ODT) %>% .[which(.==target)+1],"))")
   ##Can't use original weird name... 
@@ -170,54 +172,73 @@ UpdateModule <- function(ODT, NDT, target, switchV=""){
   TDT$Item[which(TDT$Item=="NULL")] <- "無"
   TDT[, Freq:=sum(as.numeric(Freq), na.rm=T), by=c("學校名稱", "科系名稱", "Item")]
   
-  ##1. Caculate Percentage
-  ##2. Get top N
-  ##3. Use remain percentage to caculate freq
-  TDT[, Percentage:=Freq/sum(Freq), by = c("學校名稱", "科系名稱")]
+  TDT$UniKey <- 0
+  TDT$UniKey[which(TDT$Item=="無")] <- which(TDT$Item=="無")
+  
+  TDT <- TDT %>% unique
+  TDT <- TDT[Item!=""]
+  TDT <- TDT[Item!="工讀生"]
+  TDT$UniKey <- NULL
+  
+  ##
+  TDT[, Sum:=sum(as.numeric(Freq),na.rm=T), by = c("學校名稱", "科系名稱")]
   TDT <- TDT[order(學校名稱, 科系名稱, -Freq),]
   TDT <- TDT[Item!="其他"]
   TDT
   TDT <- TDT[, head(.SD, 10), by = c("學校名稱", "科系名稱")]
-  maxNrowTDT <- nrow(TDT)
-  for(i in 1:maxNrowTDT){
-    if((TDT[i, 科系名稱]!=TDT[i+1, 科系名稱] | TDT[i, 學校名稱]!=TDT[i+1, 學校名稱]) & TDT[i, Item]!="其他"){
-      TDT <- rbindlist(list(
-        TDT[1:i], 
-        as.list(c(TDT[i, 學校名稱], TDT[i, 科系名稱], "其他", (as.numeric(TDT$Freq[i])/as.numeric(TDT$Percentage[i])*(1-sum(as.numeric(TDT$Percentage[(i-9):i])))) %>% round(., 0), 1-sum(as.numeric(TDT$Percentage[(i-9):i])))),
-        TDT[(i+1):maxNrowTDT]
-        ))
-    }
-    if(i==maxNrowTDT){
-      TDT <- rbindlist(list(
-        TDT, 
-        as.list(c(TDT[i, 學校名稱], TDT[i, 科系名稱], "其他", (as.numeric(TDT$Freq[i])/as.numeric(TDT$Percentage[i])*(1-sum(as.numeric(TDT$Percentage[(i-9):i])))) %>% round(., 0), 1-sum(as.numeric(TDT$Percentage[(i-9):i]))))
+  
+  lack <- table(paste0(TDT$學校名稱, TDT$科系名稱)) %>% as.data.frame(stringsAsFactors=F) %>% .[which(.$Freq!=10), 1]
+  lack <- TDT[paste0(學校名稱, 科系名稱) %in% lack, .(學校名稱, 科系名稱)] %>% unique
+  for(i in 1:nrow(lack)){
+    TDT <- rbindlist(list(TDT,
+    as.list(c(lack[i] %>% c %>% unlist, 
+      c("行政人員", "國內業務人員", "門市／店員／專櫃人員", "客服人員", "行政助理", "業務助理")[!(c("行政人員", "國內業務人員", "門市／店員／專櫃人員", "客服人員", "行政助理", "業務助理") %in% TDT[學校名稱==lack$學校名稱[i] & 科系名稱==lack$科系名稱[i], Item])][1], 
+      ifelse(TDT[學校名稱==lack$學校名稱[i] & 科系名稱==lack$科系名稱[i], Freq][length(TDT[學校名稱==lack$學校名稱[i] & 科系名稱==lack$科系名稱[i], Item])]>0,TDT[學校名稱==lack$學校名稱[i] & 科系名稱==lack$科系名稱[i], Freq][length(TDT[學校名稱==lack$學校名稱[i] & 科系名稱==lack$科系名稱[i], Item])],1),
+      TDT[學校名稱==lack$學校名稱[i] & 科系名稱==lack$科系名稱[i], Sum][1] %>% as.numeric + ifelse(TDT[學校名稱==lack$學校名稱[i] & 科系名稱==lack$科系名稱[i], Freq][length(TDT[學校名稱==lack$學校名稱[i] & 科系名稱==lack$科系名稱[i], Item])]>0,TDT[學校名稱==lack$學校名稱[i] & 科系名稱==lack$科系名稱[i], Freq][length(TDT[學校名稱==lack$學校名稱[i] & 科系名稱==lack$科系名稱[i], Item])],1) %>% as.numeric
       ))
+    ))
+    TDT$Sum[TDT$學校名稱==lack$學校名稱[i] & TDT$科系名稱==lack$科系名稱[i]] <- TDT[學校名稱==lack$學校名稱[i] & 科系名稱==lack$科系名稱[i], Sum][length(TDT[學校名稱==lack$學校名稱[i] & 科系名稱==lack$科系名稱[i], Sum])]
+  }
+  TDT <- rbind(TDT[Item!="無",], TDT[Item=="無",])
+  
+  tmp <- TDT[ , .(學校名稱, 科系名稱)] %>% unique
+  for(i in 1:nrow(tmp)){
+    TDT <- rbindlist(list(TDT,
+                          as.list(c(tmp %>% extract(i) %>% c %>% unlist, 
+                                    "其他", 
+                                    TDT[paste0(學校名稱, 科系名稱)==tmp %>% extract(i) %>% paste0(., collapse=""), Sum][1] %>% as.numeric - sum(as.numeric(TDT[paste0(學校名稱, 科系名稱)==tmp %>% extract(i) %>% paste0(., collapse=""), Freq])), 
+                                    TDT[paste0(學校名稱, 科系名稱)==tmp %>% extract(i) %>% paste0(., collapse=""), Sum][1]
+                          )
+                          )
+                          ))
+    cat("\r Caculating...", format(round(i/nrow(tmp)*100, 3), nsmall=3), "%")
+  }
+  
+  ##比例重算
+  ##無往後踢
+  TDT[, Percentage:=as.numeric(Freq)/as.numeric(Sum)]
+  TDT$Sum <- NULL
+  TDT$Percentage[which(TDT$Freq==0)] <- 0
+  TDT <- TDT[order(學校名稱, 科系名稱),]
+  
+  ##table(paste0(TDT$學校名稱, TDT$科系名稱)) %>% as.data.frame(stringsAsFactors=F) %>% .[which(.$Freq!=10),]
+  ##TDT[學校名稱=="XXX" & 科系名稱=="XXX"]
+  names(TDT)[3:5] <- names(ODT)[which(names(ODT)==target):(which(names(ODT)==target)+2)]
+  print("Done.")
+  return(TDT)
+  
+  
+  
+  if(F){
+    if(switchV=="職務"){
+      
+    }else if(switchV=="產業"){
+      
+    }else{
+      ##學校
     }
-    cat("\r Caculating...", format(round(i/maxNrowTDT*100, 3), nsmall=3), "%")
+    #school, department, index of cbnName and the next one
   }
-
-  
-  ## Using inefficient may be more intuitive...
-  #for(i in 1:nrow(NDT)){
-  #  NDT[i, eval(parse(text=names(NDT)[4]))]
-  #  
-  #  ODT[學校名稱==NDT$學校名稱[i] & 科系名稱==NDT$科系名稱[i] & 類別==switchV & eval(parse(text=paste0(target, "==NDT$", target, "[i]"))), eval(parse(text=names(NDT)[4]))]
-  #  ##如果沒有幼維NULL 就取代NULL?
-  #  ##這樣要先排序好:NDT 免得出現小的取代大的
-  #}
-  
-  
-  
-  
-  if(switchV=="職務"){
-    
-  }else if(switchV=="產業"){
-    
-  }else{
-    ##學校
-  }
-  #school, department, index of cbnName and the next one
-  
 }
 
 
@@ -228,4 +249,12 @@ UpdateModule <- function(ODT, NDT, target, switchV=""){
 #####################
 ## Start : Employment
 #####################
-Job1 <- DStatModule(Employment, "學校名稱", "科系名稱", "職務小類名稱")
+Job1 <- DStatModule(Employment, "SchoolName", "Department", "職務小類名稱")
+M1   <- UpdateModule(Job1, OldEmployment, "名稱(一)", "職務")
+Job2 <- DStatModule(Employment, "SchoolName", "Department", "職務小類名稱1")
+M2   <- UpdateModule(Job2, OldEmployment, "名稱(二)", "職務")
+Job3 <- DStatModule(Employment, "SchoolName", "Department", "職務小類名稱2")
+M3   <- UpdateModule(Job3, OldEmployment, "名稱(三)", "職務")
+Job4 <- DStatModule(Employment, "SchoolName", "Department", "職務小類名稱3")
+M4   <- UpdateModule(Job4, OldEmployment, "名稱(四)", "職務")
+
